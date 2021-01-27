88ed88d157358a84de391271c51cee0b05390c13
Said Laa <laarabsaid@gmail.com>
2021-01-27 21:54:52 +0100

    removed the folder containing the spring-based apps

--- Spring-Based-Apps/Cars-List/API Consumer - CarDB/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // useState is imported so we can use the hook feature : state for functional component
-import ReactDOM from 'react-dom';
-import './app.css';
-import App from './App';
-
-
-ReactDOM.render(<App/>,document.getElementById('root'));
-
--- Spring-Based-Apps/Cars-List/business-logic-server/src/main/java/io/saidlaarab/businesslogicserver/domain/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package io.saidlaarab.businesslogicserver.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerRepository extends CrudRepository<Owner, Long>{
-	
-}
